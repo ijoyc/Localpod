8328272c18c284a0796c15cb19d1271f45c358d9
chenyizhuo.yizhuoc <chenyizhuo.yizhuoc@bytedance.com>
2022-01-24 17:47:07 +0800

    First bloooooood

---
+++ Localpod.py
@@ -0,0 +1,142 @@
+import sublime
+import sublime_plugin
+import os
+from os import path
+
+PLUGIN_NAME = "Localpod"
+
+class Pod(object):
+    def __init__(self, name, textRange):
+        self.name = name
+        self.textRange = (textRange.a, textRange.b)
+
+class runlocalpodCommand(sublime_plugin.WindowCommand):
+
+    pod_list = []
+
+    first_list = []
+    second_list =[]
+    chosen_pods = []
+    replacement_map = {}
+
+    # ====== Phase 1: Build Pod List ======
+
+    def initialize(self):
+        self.build_pod_list()
+        self.chosen_pods = []
+        self.replacement_map = {}
+        self.first_list = list(map(lambda pod: pod.name, self.pod_list))
+        self.second_list = ['Done'] + self.first_list
+
+    def build_pod_list(self):
+        view = self.window.active_view()
+        all_content_range = sublime.Region(0, view.size())
+        all_content = view.substr(all_content_range)
+
+        regexp = 'pod\\s*[\'\"]([_a-zA-Z][_a-zA-Z0-9]*)[\'\"]\\s*,\\s*([\'\"][^\'\"]+[\'\"])'
+        extraction = []
+        result = view.find_all(regexp, sublime.IGNORECASE, "$1", extraction)
+        self.pod_list = list(map(lambda group: Pod(group[1], group[0]), zip(result, extraction)))
+
+    # ====== Phase 2: Find Local Pods ======
+
+    def find_local_pods(self):
+        unfound_pods = []
+        for pod in self.chosen_pods:
+            pod_path = self.configured_path_for_pod_name(pod)
+            if pod_path is None:
+                unfound_pods.append(pod)
+            else:
+                self.replacement_map[pod] = pod_path
+        if len(unfound_pods) > 0:
+            self.search_paths_for_pod_names(unfound_pods)
+
+    def configured_path_for_pod_name(self, name):
+        abs_paths = load_settings().get('abs_paths_for_pods')
+        if not name in abs_paths:
+            return None
+        pod_path = path.expanduser(abs_paths[name])
+
+        view = self.window.active_view()
+        file_dir = path.dirname(view.file_name())
+
+        return path.relpath(pod_path, file_dir)
+
+    def search_paths_for_pod_names(self, names):
+        view = self.window.active_view()
+        file_dir = path.dirname(view.file_name())
+        work_directory = path.expanduser(load_settings().get('work_directory'))
+        abs_paths = load_settings().get('abs_paths_for_pods')
+
+        for root, dirs, files in os.walk(work_directory):
+            if len(names) == 0:
+                break
+            for file in files:
+                filename, fileext = path.splitext(file)
+                if (not path.isdir(file)) and fileext == '.podspec' and filename in names:
+                    result_path = path.relpath(root, file_dir)
+                    self.replacement_map[filename] = result_path
+
+                    abs_paths[filename] = root
+                    names.remove(filename)
+
+        load_settings().set('abs_paths_for_pods', abs_paths)
+        save_settings()
+
+    # ====== User Interaction ======
+
+    def show_quick_panel(self, options, done):
+        sublime.set_timeout(lambda: self.window.show_quick_panel(options, done), 10)
+
+    def first_select(self, index):
+        if index > -1:
+            self.chosen_pods.append(self.first_list[index])
+            self.second_list.remove(self.first_list[index])
+            self.show_quick_panel(self.second_list, self.second_select)
+
+    def second_select(self, index):
+        if index == 0:
+            self.on_select_finish()
+        elif index > 0:
+            self.chosen_pods.append(self.second_list[index])
+            self.second_list.remove(self.second_list[index])
+            self.show_quick_panel(self.second_list, self.second_select)
+
+    def on_select_finish(self):
+        self.find_local_pods()
+        view = self.window.active_view()
+        pod_list = {}
+        for pod in self.pod_list:
+            pod_list[pod.name] = pod.textRange
+        view.run_command('writelocalpod', {'pod_list': pod_list, 'replacement_map': self.replacement_map})
+
+    def run(self):
+        self.initialize()
+        self.show_quick_panel(self.first_list, self.first_select)
+
+
+
+class writelocalpodCommand(sublime_plugin.TextCommand):
+    def run(self, edit, **kwargs):
+        pod_list = kwargs['pod_list']
+        replacement_map = kwargs['replacement_map']
+        for name, path in replacement_map.items():
+            if name in pod_list:
+                textRange = pod_list[name]
+                self.view.replace(edit, sublime.Region(textRange[0], textRange[1]), "pod '%s', :path => '%s'" % (name, path))
+
+
+
+# ====== Utils ======
+
+def load_settings():
+    return sublime.load_settings('%s.sublime-settings' % PLUGIN_NAME)
+
+def save_settings():
+    sublime.save_settings('%s.sublime-settings' % PLUGIN_NAME)
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,34 @@
+# Sublime Text 3 Local Pod Package
+
+
+### A [Cocoapods](https://cocoapods.org/) plugin for automatically replacing your pods with local pods
+
+### How it works
+
+Press `Cmd + Shift + P` for Mac and `Ctrl + Shift + P` for Linux/Windows.
+
+Input `Localpod` and select `Localpod: Find and Replace`, select desired pods and press `Done` to finish.
+
+To make it work and work fast, several settings are necessary.
+
+```
+{
+    "work_directory": "~/Documents/iOS",
+
+    "abs_paths_for_pods": {
+        "AFNetworking": "~/Documents/iOS/AFNetworking",
+        
+
+        "MyAwesomeToast": "~/Documents/iOS/MyApp/Libs/MyAwesomeToast",
+        "AnotherExamplePod": "~/Documents/iOS/MyApp/Libs/AnotherExamplePod",
+    },
+}
+```
+
+`work_directory` is used for searching for the local pod path. You certainly don't want this package to walk through your whole computer :]
+
+`abs_paths_for_pods` is not necessary. Anytime you specify a new pod, this package will search in `work_directory` and save the result in this map automatically once found. But if searching is too slow for you, you can manually edit this map.
+
+### Installation
+
+Install via package control
